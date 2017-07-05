b3b126fb89ba523feb0ff1dfc56a803ae911d99d
Éderson Lehugeur <ederson-lehugeur@tjmrs.jus.br>
2017-07-05 14:57:47 -0300

    Lesson 4: 14. Quiz: Delete Menu Item

--- vagrant/lesson_4/project.py
+++ vagrant/lesson_4/project.py
@@ -2,8 +2,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Restaurant, MenuItem
+from flask import Flask, render_template, request, redirect, url_for
 
-from flask import Flask, render_template
 app = Flask(__name__)
 
 engine = create_engine('sqlite:///restaurantmenu.db')
@@ -18,7 +18,7 @@ session = DBSession()
 def helloWorld():
 	return "Hello World"
 
-#@app.route('/')
+'''
 @app.route('/menu')
 def menu():
 	restaurant = session.query(Restaurant).first()
@@ -32,9 +32,9 @@ def menu():
 		output += i.description
 		output += '<br><br>'
 	return output
+'''
 
 '''
-#@app.route('/')
 @app.route('/restaurants/<int:restaurant_id>/')
 def restaurantMenu(restaurant_id):
 	restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
@@ -50,28 +50,73 @@ def restaurantMenu(restaurant_id):
 	return output
 '''
 
-#@app.route('/')
+'''	
 @app.route('/restaurants/<int:restaurant_id>/')
 def restaurantMenu(restaurant_id):
 	restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
 	items = session.query(MenuItem).filter_by(restaurant_id = restaurant.id)
 	return render_template('menu.html', restaurant = restaurant, items = items)
+'''
+
+@app.route('/restaurants/<int:restaurant_id>/menu/')
+def restaurantMenu(restaurant_id):
+    restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
+    items = session.query(MenuItem).filter_by(restaurant_id = restaurant_id)
+    return render_template('menu.html', restaurant = restaurant, items = items, restaurant_id = restaurant_id)
 	
+'''
 # Task 1: Create route for newMenuItem function here.
 @app.route('/restaurants/<int:restaurant_id>/new/')
 def newMenuItem(restaurant_id):
 	return "Page to create a new menu item. Task 1 complete!"
+'''
 
+@app.route('/restaurants/<int:restaurant_id>/new/', methods=['GET', 'POST'])
+def newMenuItem(restaurant_id):
+    if request.method == 'POST':
+        newItem = MenuItem(restaurant_id = restaurant_id, name = request.form['name'])
+        session.add(newItem)
+        session.commit()
+        return redirect(url_for('restaurantMenu', restaurant_id = restaurant_id))
+    else:
+        return render_template('newMenuItem.html', restaurant_id = restaurant_id)
+	
+'''
 # Task 2: Create route for editMenuItem function here.
 @app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/edit/')
 def editMenuItem(restaurant_id, menu_id):
 	return "Page to edit a menu item. Task 2 complete!"
+'''
+
+@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/edit/', methods=['GET', 'POST'])
+def editMenuItem(restaurant_id, menu_id):
+    editedItem = session.query(MenuItem).filter_by(id = menu_id).one()
+    if request.method == 'POST':
+        if request.form['name']:
+            editedItem.name = request.form['name']
+        session.add(editedItem)
+        session.commit()
+        return redirect(url_for('restaurantMenu', restaurant_id = restaurant_id))
+    else:
+        return render_template('editMenuItem.html', restaurant_id = restaurant_id, menu_id = menu_id, item = editedItem)
 	
-# Task 2: Create route for deleteMenuItem function here.
+'''
+# Task 3: Create route for deleteMenuItem function here.
 @app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/delete/')
 def deleteMenuItem(restaurant_id, menu_id):
 	return "Page to delete a menu item. Task 3 complete!"
-
+'''
+	
+@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/delete/', methods=['GET', 'POST'])
+def deleteMenuItem(restaurant_id, menu_id):
+    itemToDelete = session.query(MenuItem).filter_by(id = menu_id).one()
+    if request.method == 'POST':
+        session.delete(itemToDelete)
+        session.commit()
+        return redirect(url_for('restaurantMenu', restaurant_id = restaurant_id))
+    else:
+        return render_template('deleteMenuItem.html', restaurant_id = restaurant_id, item = itemToDelete)
+	
 if __name__ == '__main__':
     app.debug = True
     app.run(host = '0.0.0.0', port = 5000)
