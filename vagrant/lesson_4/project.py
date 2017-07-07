eb83eed9697c4d8f030b670ddf529354a0136010
Éderson Lehugeur <ederson-lehugeur@tjmrs.jus.br>
2017-07-07 16:01:13 -0300

    Lesson 4: 17. Quiz: Styling

--- vagrant/lesson_4/project.py
+++ vagrant/lesson_4/project.py
@@ -60,21 +60,54 @@ def restaurantMenu(restaurant_id):
 	return render_template('menu.html', restaurant = restaurant, items = items)
 '''
 
-'''
 @app.route('/')
 @app.route('/restaurants/')
-def restaurantMenu():
+def restaurants():
     restaurants = session.query(Restaurant).all()
-    #items = session.query(MenuItem).filter_by(restaurant_id = restaurant_id)
     return render_template('restaurants.html', restaurants = restaurants)
-'''
+	
+@app.route('/restaurants/new/', methods = ['GET', 'POST'])
+def newRestaurant():
+	if request.method == 'POST':
+		newRestaurant = Restaurant(name = request.form['name'])
+		session.add(newRestaurant)
+		session.commit()
+		flash("Restaurant %s created!" % newRestaurant.name)
+		return redirect(url_for('restaurants'))
+	else:
+		return render_template('newRestaurant.html')
+	
+@app.route('/restaurants/<int:restaurant_id>/edit/', methods = ['GET', 'POST'])
+def editRestaurant(restaurant_id):
+	editedRestaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
+	if request.method == 'POST':
+		if request.form['name']:
+			editedRestaurant.name = request.form['name']
+		session.add(editedRestaurant)
+		session.commit()
+		flash("Restaurant '%s' has been edited!" % editedRestaurant.name)
+		return redirect(url_for('restaurants'))
+	else:
+		return render_template('editRestaurant.html', restaurant = editedRestaurant)
+
+@app.route('/restaurants/<int:restaurant_id>/delete/', methods = ['GET', 'POST'])
+def deleteRestaurant(restaurant_id):
+    restaurantToDelete = session.query(Restaurant).filter_by(id = restaurant_id).one()
+    if request.method == 'POST':
+        nameRestaurant = restaurantToDelete.name
+        session.delete(restaurantToDelete)
+        session.commit()
+        flash("Restaurant '%s' has been deleted!" % nameRestaurant)
+        return redirect(url_for('restaurants'))
+    else:
+        return render_template('deleteRestaurant.html', restaurant_id = restaurant_id, item = restaurantToDelete)
 
 @app.route('/restaurants/<int:restaurant_id>/menu/')
 def restaurantMenu(restaurant_id):
     restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
     items = session.query(MenuItem).filter_by(restaurant_id = restaurant_id)
-    return render_template('menu.html', restaurant = restaurant, items = items, restaurant_id = restaurant_id)
-	
+    return render_template('menu.html', restaurant = restaurant, items = items)
+
 '''
 # Task 1: Create route for newMenuItem function here.
 @app.route('/restaurants/<int:restaurant_id>/new/')
@@ -85,7 +118,8 @@ def newMenuItem(restaurant_id):
 @app.route('/restaurants/<int:restaurant_id>/new/', methods = ['GET', 'POST'])
 def newMenuItem(restaurant_id):
 	if request.method == 'POST':
-		newItem = MenuItem(restaurant_id = restaurant_id, name = request.form['name'])
+		newItem = MenuItem(restaurant_id = restaurant_id, name = request.form['name'], description = request.form['description'], 
+			price = request.form['price'], course = request.form['course'])
 		session.add(newItem)
 		session.commit()
 		flash("Menu item '%s' created!" % newItem.name)
@@ -106,6 +140,12 @@ def editMenuItem(restaurant_id, menu_id):
 	if request.method == 'POST':
 		if request.form['name']:
 			editedItem.name = request.form['name']
+		if request.form['description']:
+			editedItem.description = request.form['description']
+		if request.form['price']:
+			editedItem.price = request.form['price']
+		if request.form['course']:
+			editedItem.course = request.form['course']
 		session.add(editedItem)
 		session.commit()
 		flash("Menu item '%s' has been edited!" % editedItem.name)
@@ -123,8 +163,8 @@ def deleteMenuItem(restaurant_id, menu_id):
 @app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/delete/', methods=['GET', 'POST'])
 def deleteMenuItem(restaurant_id, menu_id):
     itemToDelete = session.query(MenuItem).filter_by(id = menu_id).one()
-    nameItem = itemToDelete.name
     if request.method == 'POST':
+        nameItem = itemToDelete.name
         session.delete(itemToDelete)
         session.commit()
         flash("Menu item '%s' has been deleted!" % nameItem)
